f22620924d4efe2af890582ee114ef0af004d666
James Frandsen <copin123@gmail.com>
2014-02-02 22:19:19 -0500

    adding initial builder pattern for Family.java.  i still need to change it to add a list of family members

--- src/main/java/FamilyMembers/Family.java
+++ src/main/java/FamilyMembers/Family.java
@@ -1,44 +1,58 @@
 package FamilyMembers;
 
+import java.util.ArrayList;
+
 public class Family {
 	public int familyId;
 	public String familyName;
 	public String familyParentName1;
 	public String familyParentName2;
 	public String familyLastName;
-	public int getFamilyId() {
-		return familyId;
+	public ArrayList<FamilyMember> familyMembers;
+
+	public static class Builder {
+		public int familyId;
+		public String familyName;
+		public String familyParentName1;
+		public String familyParentName2;
+		public String familyLastName;
+
+		public Builder familyId(int familyId) {
+			this.familyId = familyId;
+			return this;
+		}
+
+		public Builder familyName(String familyName) {
+			this.familyName = familyName;
+			return this;
+		}
+
+		public Builder familyParentName1(String familyParentName1) {
+			this.familyParentName1 = familyParentName1;
+			return this;
+		}
+
+		public Builder familyParentName2(String familyParentName2) {
+			this.familyParentName2 = familyParentName2;
+			return this;
+		}
+
+		public Builder familyLastName(String familyLastName) {
+			this.familyLastName = familyLastName;
+			return this;
+		}
+
+		public Family build() {
+			return new Family(this);
+		}
+
 	}
-	public Family setFamilyId(int familyId) {
-		this.familyId = familyId;
-		return this;
-	}
-	public String getFamilyName() {
-		return familyName;
-	}
-	public Family setFamilyName(String familyName) {
-		this.familyName = familyName;
-		return this;
-	}
-	public String getFamilyParentName1() {
-		return familyParentName1;
-	}
-	public Family setFamilyParentName1(String familyParentName1) {
-		this.familyParentName1 = familyParentName1;
-		return this;
-	}
-	public String getFamilyParentName2() {
-		return familyParentName2;
-	}
-	public Family setFamilyParentName2(String familyParentName2) {
-		this.familyParentName2 = familyParentName2;
-		return this;
-	}
-	public String getFamilyLastName() {
-		return familyLastName;
-	}
-	public Family setFamilyLastName(String familyLastName) {
-		this.familyLastName = familyLastName;
-		return this;
+
+	private Family(Builder builder) {
+		familyId = builder.familyId;
+		familyName = builder.familyName;
+		familyParentName1 = builder.familyParentName1;
+		familyParentName2 = builder.familyParentName2;
+		familyLastName = builder.familyLastName;
 	}
 }
